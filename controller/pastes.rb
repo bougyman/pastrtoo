461a90ec643e2b54bd9c746742b26403a64040b2
none <linuxhelp@Esther.(none)>
2008-12-24 11:45:19 -0600

    fixed channel showing up in annotations

--- controller/pastes.rb
+++ controller/pastes.rb
@@ -14,39 +14,45 @@ class PastesController < Controller
     @paste_entries = PasteEntry.order(:id.desc).filter("paste_body is not null").paginate(1,10)
   end
 
-  def annotate(paste_id)
-    require "captcha"
+  def common_annotate(paste_id)
     @title = "Annotating Paste #{paste_id}"
     @annotation = Annotation.new
-    @paste_entry = PasteEntry[paste_id]
     @captcha = CAPTCHA::Web.new(:image_dir => File.join(File.dirname(__FILE__), "..", "public/img"), :font => "/usr/share/fonts/truetype/ttf-bitstream-vera/Vera.ttf")
     @captcha.image
     @captcha.clean
+  end
+  private :common_annotate
+
+  def annotate(paste_id)
+    require "captcha"
+    @paste_entry = PasteEntry[paste_id]
+    common_annotate(paste_id)
   end
 
   def add_annotation(paste_id)
     require "captcha"
     captcha_digest = request["captcha_digest"]
     captcha_key    = request["captcha_key"]
+    @paste_entry = PasteEntry[paste_id]
     unless CAPTCHA::Web.is_valid(captcha_key, captcha_digest)
       flash[:ERRORS] = "Invalid Captcha"
-      @title = "Annotating Paste #{paste_id}"
-      @annotation = Annotation.new({:paste_entry_id => paste_id}.merge(request["annotation"]))
-      @paste_entry = PasteEntry[paste_id]
-      @captcha = CAPTCHA::Web.new(:image_dir => File.join(File.dirname(__FILE__), "..", "public/img"), :font => "/usr/share/fonts/truetype/ttf-bitstream-vera/Vera.ttf")
-      @captcha.image
-      @captcha.clean
+      common_annotate(paste_id)
       render_template("annotate.haml")
     else
-      annotation_params = request["annotation"]
-      nickname = annotation_params[:nickname].to_s
+      nickname = request[:nickname].to_s
       nickname = "Anonymous Coward" if nickname == ""
       unless paster = Paster[:nickname => nickname]
         paster = Paster.create(:nickname => nickname)
       end
-      annotation = Annotation.create({:paste_entry_id => paste_id, :paster_id => paster.id}.merge(annotation_params))
-      flash[:INFO] = "Annotation created"
-      redirect R("/#{paste_id}")
+      annotation = Annotation.create({:channel => @paste_entry.channel, :paste_entry_id => paste_id, :paster_id => paster.id}.merge(request["annotation"]))
+      if annotation.nil?
+        flash[:ERRORS] = "Failed to annotate!"
+        common_annotate(paste_id)
+        return_template("annotate.haml")
+      else
+        flash[:INFO] = "Annotation created"
+        redirect R("/#{paste_id}")
+      end
     end
   end
 
