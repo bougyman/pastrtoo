62335a8f7d28377267a4fe5f2e61ef7269d8e25f
none <linuxhelp@eagle.(none)>
2009-04-06 15:26:48 -0500

    fixed some bugs with requires and pulled from #freeswitch

--- model/user.rb
+++ model/user.rb
@@ -2,7 +2,7 @@ class User < Sequel::Model
   one_to_many :pasters, :one_to_one => true
   def set_pass(pass)
     require "digest/md5"
-    require "lib/pastr_it"
+    require "pastr_it"
     self.password = Digest::MD5.hexdigest([nickname, PastrIt::REALM, pass].join(':'))
   end
 end
--- pastr_bot/leaves/pastr/controller.rb
+++ pastr_bot/leaves/pastr/controller.rb
@@ -15,7 +15,8 @@ class Controller < Autumn::Leaf
 
   def help_command(stem, sender, reply_to, msg)
     ".hitme <optional title> - in a channel and i'll message you a paste link.  You paste stuff at that link and it tells the channel about it\n" + 
-    ".pastr <optional title> - exact same thing, more catchy name"
+    ".pastr <optional title> - exact same thing, more catchy name\n" +
+    ".register <password> - register to be able to use http://pastr.it/admin functions (Freenode only, private message only)"
   end
 
   def hello_command(stem, sender, reply_to, msg)
@@ -28,9 +29,27 @@ class Controller < Autumn::Leaf
     paster = ::Paster.find_or_create(:nickname => sender[:nick])
     paste_title = msg.blank? ? "Pastr by #{sender[:nick]}" : msg
     paste_entry = ::PasteEntry.create(:network => stem.options[:server_id], :paster_id => paster.id, :reply_to => PASTR_SOCKET, :title => paste_title, :channel => reply_to, :filter_id => Filter.id_for(reply_to, stem.options[:server_id]))
-    stem.message "Paste to http://pastr.it/#{paste_entry.id}/#{key}", sender[:nick]
-    stem.message("Sent http://pastr.it/#{paste_entry.id}/#{key} to #{sender[:nick]}", reply_to) if reply_to.match(/lighttpd/)
+    stem.message paste_entry.paste_link, sender[:nick]
+    stem.message("Sent #{paste_entry.paste_link} to #{sender[:nick]}", reply_to) if reply_to.match(/lighttpd/)
     nil
+  end
+
+  def register_command(stem, sender, reply_to, msg)
+    if stem[:server_id] != 'Freenode'
+      ".register is only available on Freenode"
+    else 
+      if reply_to.match(/^[#&+]/)
+        "Trying to register in public?  Why not just give everyone your social security number while you're at it?"
+      elsif sender[:nick] == reply_to
+        paster = ::Paster.find_or_create(:nickname => reply_to)
+        user = User.find_or_create(:nickname => reply_to)
+        user.set_pass(msg.strip)
+        user.save
+        paster.user = user
+        paster.save
+        "User #{reply_to} (uid #{user.id}): password set to #{msg.strip}"
+      end
+    end
   end
 
   def pastr_command(stem, sender, reply_to, msg)
