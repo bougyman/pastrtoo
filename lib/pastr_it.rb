2c28d1948dc1f50d267a0f00e8d8196000ead8f6
TJ Vanderpoel <bougy.man@gmail.com>
2009-04-12 09:05:20 -0500

    added edit and annotate to pastr-it

--- lib/pastr_it.rb
+++ lib/pastr_it.rb
@@ -2,8 +2,11 @@ require "optparse"
 class PastrIt
   VERSION = '0.1.7'
   REALM = 'Pastr Registered User'
-  PasteLink = "http://pastr.it/new"
-  attr_accessor :password, :filename, :title, :network, :channel, :language, :username
+  PastrHome = "http://pastr.it"
+  PastrNew  = "%s/%s" % [PastrHome, :new]
+  PastrEdit = "%s/%s" % [PastrHome, :edit]
+  PastrNote = "%s/%s" % [PastrHome, :annotate]
+  attr_accessor :password, :filename, :title, :network, :channel, :language, :username, :annotate_id, :pastr_id, :no_body
   def initialize(args = nil)
     @network, @username = "Freenode", ENV["USER"]
     @args = args || ARGV
@@ -13,26 +16,38 @@ class PastrIt
   def self.pastr_it(args)
     me = PastrIt.new(args)
     me.parse_args
-    me.pastr_it
+    if me.annotate_id
+      me.annotate_it
+    elsif me.pastr_id
+      me.edit_it
+    else
+      me.pastr_it
+    end
   end
 
   def parse_args
     return @opts if @opts
     @opts = OptionParser.new
-    @opts.banner = "\nUsage: pastr-it [options] FILE\n"
-    @opts.separator "\tTo paste from STDIN (instead of a file), leave off FILE, and you'll need to auth with a ~/.netrc"
+    @opts.banner = "\nUsage: pastr-it [options] FILE"
+
+    @opts.separator "--- Common Pastr Options"
+    @opts.separator " To paste from STDIN (instead of a file), leave off FILE, and you'll need to auth with a ~/.netrc"
     @opts.on("-u", "--username USERNAME", "Your username (Default: #{@username})") { |foo| @username = foo }
     @opts.on("-c", "--channel CHANNEL", "IRC Channel for this Pastr (Default: same as username)") { |foo| @channel = foo }
     @opts.separator "\tWhen using your username as the channel argument, the paste will be private"
     @opts.on("-n", "--network NETWORK", "IRC Network for this Pastr (Default: #{network})") { |foo| @network = foo }
     @opts.on("-l", "--language LANGUAGE", "Language to use for syntax highlighting") { |foo| @language = foo }
     @opts.on("-t", "--title TITLE", "Title of this paste (Default: Filename or 'Pastr by #{username}')") { |foo| @title = foo }
-    # coming soon
-    #@opts.on("-i", "--id PID", "ID Of paste to edit (instead of making a new paste)") { |foo| @pastr_id = foo }
-    #@opts.on("-a", "--annotate PID", "ID Of paste to annotate (incompatible with -i)") { |foo| @annotate_id = foo }
-    @opts.separator "\n---------------\n"
-    @opts.on("-L", "--list", "List supported languages") { |foo| @list_langs = true }
 
+    @opts.separator "--- Editing"
+    @opts.on("-e", "--edit PASTR_ID", "ID Of paste to edit (instead of making a new paste)") { |foo| @pastr_id = foo }
+    @opts.on("-N", "--no-body", "Just edit metadata (channel, language, etc), not the paste body") { |foo| @no_body = true }
+
+    @opts.separator "--- Annotate"
+    @opts.on("-a", "--annotate PASTR_ID", "ID Of paste to annotate (incompatible with -e/--edit.  -n/--network, and -c/--channel ignored.)") { |foo| @annotate_id = foo }
+
+    @opts.separator "--- Informational"
+    @opts.on("-L", "--list", "List supported languages") { |foo| @list_langs = true }
     @opts.on("-v", "--version", "Print pastr version") { |foo| @version_only = true }
     @opts.on_tail("-h", "--help", "Show this message") do
       puts @opts
@@ -54,7 +69,25 @@ class PastrIt
     if @filename.nil? and STDIN.isatty
       $stderr.puts "No Input on STDIN and no filename given, what am I supposed to paste?" 
       exit 1
-    end
+    end unless @pastr_id
+  end
+
+  def annotate_it
+    form = {'paste_body' => paste_body}
+    form["title"] = title if title
+    form["language"] = language if language
+    puts http_request(:form => form, :url => PastrNote).content
+  end
+
+  def edit_it
+    form = {'network' => network, 'channel' => channel, 'paste_body' => paste_body}
+    form = {}
+    form['network']     = network if network
+    form['channel']     = channel if channel
+    form["title"]       = title if title
+    form["language"]    = language if language
+    form['paste_body']  = paste_body unless no_body
+    puts http_request(:form => form, :url => PastrEdit).content
   end
 
   def pastr_it
@@ -67,28 +100,31 @@ class PastrIt
   private
   def http_request(args)
     form = args[:form] || nil
-    url = args[:url] || PasteLink
+    url = args[:url] || PastrNew
+    auth = args[:auth] || true
     require "httpclient"
-    check_netrc
-    check_password
+    if auth
+      check_netrc
+      check_password
+    end
     client = HTTPClient.new(ENV["HTTP_PROXY"] ? ENV["HTTP_PROXY"] : ENV["http_proxy"])
     # Have to do this to get a valid cookie with the server before we auth (lame)
-    res = client.get("http://pastr.it")
+    res = client.get(PastrHome)
     if res.status != 200
-      puts "Cannot access http://pastr.it. Webserver said Status: #{res.status} -> #{res.reason}"
+      puts "Cannot access #{PastrHome}. Webserver said Status: #{res.status} -> #{res.reason}"
       exit 1
     end
     if form
       # Now set auth and post
-      client.set_auth(url, username.strip, password.strip)
+      client.set_auth(url, username.strip, password.strip) if auth
       res = client.post(url, form)
     else
       # Or set auth and get
-      client.set_auth(url, username.strip, password.strip)
+      client.set_auth(url, username.strip, password.strip) if auth
       res = client.get(url)
     end
     if res.status != 200
-      puts "An error occurred posting to#{PasteLink}. Webserver said Status: #{res.status} -> #{res.reason}"
+      puts "An error occurred posting to #{url}. Webserver said Status: #{res.status} -> #{res.reason}"
       exit 1
     end
     res
@@ -134,7 +170,6 @@ class PastrIt
       end
     end 
   end
-
 
   def paste_body
     return @paste_body if @paste_body
